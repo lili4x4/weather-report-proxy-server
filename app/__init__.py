f71b8dca00c019ecbc200713a69cfea5e0002616
Liliana <liliana.m.parra@gmail.com>
2023-04-26 18:37:32 -0400

    Put app outside of createapp function

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 
 
-def create_app(test_config=None):
-    app = Flask(__name__)
-    CORS(app)
-    app.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-    from .routes import proxy_bp
-    app.register_blueprint(proxy_bp)
+from .routes import proxy_bp
+app.register_blueprint(proxy_bp)
 
-    return app
